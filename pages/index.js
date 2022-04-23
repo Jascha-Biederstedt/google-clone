595f3ca5e16de00ea329f4e541dbc80c03c52500
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-23 13:05:00 +0200

    Add main section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SearchIcon, MicrophoneIcon } from '@heroicons/react/solid';
 
 import Header from '../components/Header';
 
@@ -13,7 +14,24 @@ export default function Home() {
 
       <Header />
 
-      <main></main>
+      <main>
+        <form className="flex flex-col items-center mt-40">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+            alt="Google Logo"
+            className="w-72 object-cover"
+          />
+          <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gary-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+            <SearchIcon className="h-5 text-gray-500 mr-3" />
+            <input type="text" className="flex-grow focus:outline-none" />
+            <MicrophoneIcon className="h-5 text-gray-500 mr-3" />
+          </div>
+          <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+            <button className="btn">Input Search</button>
+            <button className="btn">Random Search</button>
+          </div>
+        </form>
+      </main>
 
       <footer></footer>
     </div>
