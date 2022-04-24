c59bd10af8de21b662ca4b32ffe43290cd4c310f
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-24 11:55:45 +0200

    Implement getting data from Google API and adding dummy data

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,27 @@ import React from 'react';
 import Head from 'next/head';
 
 import SearchHeader from '../components/SearchHeader';
+import dummyData from '../data/dummyData';
 
-const search = () => {
+export const getServerSideProps = async context => {
+  const isDev = true;
+  const searchResults = isDev
+    ? dummyData
+    : await fetch(`https://www.googleapis.com/customsearch/v1?key=${
+        process.env.GOOGLE_API_KEY
+      }&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${context.query.term}${
+        context.query.searchType && '&searchType=image'
+      }
+  `).then(res => res.json());
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+};
+
+const search = ({ searchResults }) => {
   return (
     <div>
       <Head>
